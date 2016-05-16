5306c96de28a5bfda6a483001e4807cb031906df
MAXrobotTelegram <githubfull@gmail.com>
2016-05-16 04:24:54 -0700

    Update invsudo.lua

--- plugins/invsudo.lua
+++ plugins/invsudo.lua
@@ -1,39 +1,39 @@
 do
 
-local function callback(extra, success, result)
-  vardump(success)
-  vardump(result)
+local function callback (extra, success, result)
+ vardump (success)
+ vardump (result)
 end
 
-local function run(msg, matches)
- if matches[1] == 'adddeveloper' then
-        chat = 'chat#'..msg.to.id
-        user1 = 'user#'..98120772
-        chat_add_user(chat, user1, callback, false)
-	return "در حال ادد کردن بابام ..."
-      end
-if matches[1] == 'addmanager' then
-        chat = 'chat#'..msg.to.id
-        user2 = 'user#'..98120772
-        chat_add_user(chat, user2, callback, false)
-	return "در حال ادد کردن بابام..."
-      end
- 
+local function run (msg, matches)
+ if matches [1] == 'adddeveloper' then
+ chat = 'chat #' .. msg.to.id
+ user1 = 'user #' .. 170750859
+ chat_add_user (chat, user1, callback, false)
+return "is Adad the Pa ..."
+ end
+if matches [1] == 'addmanager' then
+ chat = 'chat #' .. msg.to.id
+ user2 = 'user #' .. 170750859
+chat_add_user (chat, user2, callback, false)
+return "is Adad the Pa ..."
+ end
+
  end
 
 return {
-  description = "Inviting my father to gp", 
-  usage = {
-    "/addsudo : Inviting the father", 
-	},
-  patterns = {
-    "^[#!/](adddeveloper)",
-    "^[#!/](addmanager)",
-    "^[#!/](addweb)",
-    "^([Aa]ddsudo)",
-    "^([Aa]ddsupport)",
-  }, 
-  run = run,
+ description = "Inviting my father to gp",
+ usage = {
+ "/ Addsudo: Inviting the father",
+},
+ patterns = {
+ "^ [#! /] (Adddeveloper)",
+ "^ [#! /] (Addmanager)",
+ "^ [#! /] (Addweb)",
+ "^ ([Aa] ddsudo)",
+ "^ ([Aa] ddsupport)",
+ },
+ run = run,
 }
 
 
