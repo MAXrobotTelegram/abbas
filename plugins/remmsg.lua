080d84fed5e2bbf1de1b961d253026669691de03
MAXrobotTelegram <githubfull@gmail.com>
2016-05-16 04:35:14 -0700

    Update remmsg.lua

--- plugins/remmsg.lua
+++ plugins/remmsg.lua
@@ -1,30 +1,30 @@
-local function history(extra, suc, result)
-for i=1, #result do
-delete_msg(result[i].id, ok_cb, false)
+local function history (extra, suc, result)
+for i = 1, #result do
+delete_msg (result [i] .id, ok_cb, false)
 end
-if tonumber(extra.con) == #result then
-send_msg(extra.chatid, ''..#result..' پیام اخیر سوپر گروه حذف شد', ok_cb, false)
+if tonumber (extra.con) == #result then
+send_msg (extra.chatid, '' .. # result .. 'recent message was deleted ', ok_cb, false)
 else
-send_msg(extra.chatid, 'تمام پیام های سوپر گروه حذف شد', ok_cb, false)
+send_msg (extra.chatid, 'All messages will delete super groups', ok_cb, false)
 end
 end
-local function run(msg, matches)
-if matches[1] == 'msgrem' and is_owner(msg) then
-            if msg.to.type == 'channel' then
-            if tonumber(matches[2]) > 9999 or tonumber(matches[2]) < 1 then
-            return "خطا عدد انتخاب شده باید زیر 100 باشد"
-            end
-            get_history(msg.to.peer_id, matches[2] + 1 , history , {chatid = msg.to.peer_id, con = matches[2]})
-        else
-                         return "فقط مخصوص سوپرگروه می باشد"
-        end
+local function run (msg, matches)
+if matches [1] == 'msgrem' and is_owner (msg) then
+            if msg.to.type == 'channel' then
+            if tonumber (matches [2])> 9999 or tonumber (matches [2]) <1 then
+            return "error selected number should be below 100."
+            end
+            get_history (msg.to.peer_id, matches [2] + 1, history, {chatid = msg.to.peer_id, con = matches [2]})
+        else
+                         return "is only for Super group"
+        end
 else
-return "فقط مخصوص مدیران گروه می باشد"
+return "is only for managers"
 end
 end
 return {
-    patterns = {
-        '^[!/#](msgrem) (%d*)$'
-    },
-    run = run
+    patterns = {
+        '^ [! / #] (Msgrem) (% d *) $'
+    },
+    run = run
 }
