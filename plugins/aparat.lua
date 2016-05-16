94350bf81f46cd4fc453fc4243b8e06e77df7456
MAXrobotTelegram <githubfull@gmail.com>
2016-05-16 04:53:02 -0700

    Update aparat.lua

--- plugins/aparat.lua
+++ plugins/aparat.lua
@@ -1,30 +1,30 @@
---[[
+- [[
 
 #
-#     @GPMOD
-#   @Dragon_Born
-#      
+# @GPMOD
+# @Dragon_Born
+#
 
 ]]
 
-local function run(msg, matches)
-	if matches[1]:lower() == 'aparat' then
-		local url = http.request('http://www.aparat.com/etc/api/videoBySearch/text/'..URL.escape(matches[2]))
-		local jdat = json:decode(url)
+local function run (msg, matches)
+if matches [1]: lower () == 'aparat' then
+local url = http.request ( 'http://www.aparat.com/etc/api/videoBySearch/text/'..URL.escape (matches [2]))
+local jdat = json: decode (url)
 
-		local items = jdat.videobysearch
-		text = 'نتیجه جستوجو در آپارات: \n'
-		for i = 1, #items do
-		text = text..'\n'..i..'- '..items[i].title..'  -  تعداد بازدید: '..items[i].visit_cnt..'\n    لینک: aparat.com/v/'..items[i].uid
-		end
-		text = text..'\n\n@TeleNERObot'
-		return text
-	end
+local items = jdat.videobysearch
+text = 'search results on Apparatus: \ n'
+for i = 1, #items do
+text = text .. '\ n' .. i ..'- '..items [i] .title ..' - Hits: '..items [i] .visit_cnt ..' \ n link: aparat. com / v / '.. items [i] .uid
+end
+text = text .. '\ n \ n @ TeleNERObot'
+return text
+end
 end
 
 return {
-   patterns = {
-"^[/!](aparat) (.*)$",
-   },
-   run = run
+    patterns = {
+"^ [/!] (Aparat) (. *) $",
+    },
+    run = run
 }
